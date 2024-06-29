7a0f9635ee0f1f7be7252a935a081de5fbbefd84
Alex Morehead <acmwhb@missouri.edu>
2024-06-29 14:22:45 -0500

    Add new unit test for filtered (random) data parsing

--- tests/test_data_parsing.py
+++ tests/test_data_parsing.py
@@ -136,5 +136,68 @@ def test_unfiltered_random_mmcif_objects_parsing(
             raise error
 
 
-if __name__ == "__main__":
-    pytest.main(["-s", __file__])
+@pytest.mark.parametrize("mmcif_dir", [os.path.join("data", "pdb_data", "mmcifs")])
+@pytest.mark.parametrize("num_random_complexes_to_parse", [500])
+@pytest.mark.parametrize("random_seed", [1])
+def test_filtered_random_mmcif_objects_parsing(
+    mmcif_dir: str,
+    num_random_complexes_to_parse: int,
+    random_seed: int,
+) -> None:
+    """Tests mmCIF file parsing and `Biomolecule` object creation for a random batch
+    of filtered mmCIF files.
+
+    :param mmcif_dir: The directory containing all (filtered) PDB mmCIF files.
+    :param num_random_complexes_to_parse: The number of random complexes to parse.
+    :param random_seed: The random seed for reproducibility.
+    """
+    random.seed(random_seed)
+
+    if not os.path.exists(mmcif_dir):
+        pytest.skip(f"Directory '{mmcif_dir}' does not exist.")
+
+    parsing_errors = []
+    failed_complex_indices = []
+    failed_random_complex_filepaths = []
+    mmcif_subdirs = [
+        os.path.join(mmcif_dir, subdir)
+        for subdir in os.listdir(mmcif_dir)
+        if os.path.isdir(os.path.join(mmcif_dir, subdir))
+    ]
+    for complex_index in range(num_random_complexes_to_parse):
+        random_mmcif_subdir = random.choice(mmcif_subdirs)
+        mmcif_subdir_files = [
+            os.path.join(random_mmcif_subdir, mmcif_subdir_file)
+            for mmcif_subdir_file in os.listdir(random_mmcif_subdir)
+            if os.path.isfile(os.path.join(random_mmcif_subdir, mmcif_subdir_file))
+            and mmcif_subdir_file.endswith(".cif")
+        ]
+
+        random_complex_filepath = random.choice(mmcif_subdir_files)
+        complex_id = os.path.splitext(os.path.basename(random_complex_filepath))[0]
+
+        if not os.path.exists(random_complex_filepath):
+            print(f"File '{random_complex_filepath}' does not exist.")
+            continue
+
+        with open(random_complex_filepath, "r") as f:
+            mmcif_string = f.read()
+
+        parsing_result = mmcif_parsing.parse(
+            file_id=complex_id,
+            mmcif_string=mmcif_string,
+            auth_chains=True,
+            auth_residues=True,
+        )
+
+        if parsing_result.mmcif_object is None:
+            parsing_errors.append(list(parsing_result.errors.values())[0])
+            failed_complex_indices.append(complex_index)
+            failed_random_complex_filepaths.append(random_complex_filepath)
+
+    if parsing_result.mmcif_object is None:
+        print(
+            f"Failed to parse {len(parsing_errors)} files at indices {failed_complex_indices}: '{failed_random_complex_filepaths}'."
+        )
+        for error in parsing_errors:
+            raise error
