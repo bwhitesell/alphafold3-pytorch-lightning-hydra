37220ee73e717cbb9184186cb2a7f1c89656f03c
Alex Morehead <alex.morehead@gmail.com>
2024-07-18 17:39:30 -0500

    Make detection of training mode more robust

--- tests/test_train.py
+++ tests/test_train.py
@@ -13,83 +13,86 @@ from tests.helpers.run_if import RunIf
 os.environ["TYPECHECK"] = "True"
 
 
-# NOTE: this test is skipped for the sake of CI speed
+def test_train_fast_dev_run(cfg_train: DictConfig) -> None:
+    """Run for 1 train, val and test step.
 
-# def test_train_fast_dev_run(cfg_train: DictConfig) -> None:
-#     """Run for 1 train, val and test step.
-
-#     :param cfg_train: A DictConfig containing a valid training configuration.
-#     """
-#     HydraConfig().set_config(cfg_train)
-#     with open_dict(cfg_train):
-#         cfg_train.trainer.max_steps = 1
-#         cfg_train.trainer.fast_dev_run = True
-#         cfg_train.trainer.accelerator = "cpu"
-#     train(cfg_train)
-
-# NOTE: this test is skipped for the sake of CI speed
-
-# @RunIf(min_gpus=1)
-# def test_train_fast_dev_run_gpu(cfg_train: DictConfig) -> None:
-#     """Run for 1 train, val and test step on GPU.
-
-#     :param cfg_train: A DictConfig containing a valid training configuration.
-#     """
-#     HydraConfig().set_config(cfg_train)
-#     with open_dict(cfg_train):
-#         cfg_train.trainer.max_steps = 1
-#         cfg_train.trainer.fast_dev_run = True
-#         cfg_train.trainer.accelerator = "gpu"
-#     train(cfg_train)
+    :param cfg_train: A DictConfig containing a valid training configuration.
+    """
+    pytest.skip("This test is skipped for the sake of CI speed.")
+    HydraConfig().set_config(cfg_train)
+    with open_dict(cfg_train):
+        cfg_train.trainer.max_steps = 1
+        cfg_train.trainer.fast_dev_run = True
+        cfg_train.trainer.accelerator = "cpu"
+    train(cfg_train)
 
 
-# # NOTE: The current AlphaFold 3 implementation does not support mixed-precision training
-# # due to the `torch.svd` error: `RuntimeError: "svd_cuda_gesvdjBatched" not implemented for 'Half'`.
-# # This test is commented out until the issue is resolved.
+@RunIf(min_gpus=1)
+def test_train_fast_dev_run_gpu(cfg_train: DictConfig) -> None:
+    """Run for 1 train, val and test step on GPU.
 
-# @RunIf(min_gpus=1)
-# @pytest.mark.slow
-# def test_train_steps_gpu_amp(cfg_train: DictConfig) -> None:
-#     """Train 2 steps on GPU with mixed-precision.
+    :param cfg_train: A DictConfig containing a valid training configuration.
+    """
+    pytest.skip("This test is skipped for the sake of CI speed.")
+    HydraConfig().set_config(cfg_train)
+    with open_dict(cfg_train):
+        cfg_train.trainer.max_steps = 1
+        cfg_train.trainer.fast_dev_run = True
+        cfg_train.trainer.accelerator = "gpu"
+    train(cfg_train)
 
-#     :param cfg_train: A DictConfig containing a valid training configuration.
-#     """
-#     HydraConfig().set_config(cfg_train)
-#     with open_dict(cfg_train):
-#         cfg_train.trainer.max_steps = 2
-#         cfg_train.trainer.accelerator = "gpu"
-#         cfg_train.trainer.precision = 16
-#     train(cfg_train)
 
-# NOTE: this test is skipped for the sake of CI speed
+# NOTE: The current AlphaFold 3 implementation does not support mixed-precision training
+# due to the `torch.svd` error: `RuntimeError: "svd_cuda_gesvdjBatched" not implemented for 'Half'`.
+# This test is skipped until the issue is resolved.
 
-# @pytest.mark.slow
-# def test_train_steps_double_val_loop(cfg_train: DictConfig) -> None:
-#     """Train 2 steps with validation loop twice per epoch.
 
-#     :param cfg_train: A DictConfig containing a valid training configuration.
-#     """
-#     HydraConfig().set_config(cfg_train)
-#     with open_dict(cfg_train):
-#         cfg_train.trainer.max_steps = 2
-#         cfg_train.trainer.val_check_interval = 0.5
-#     train(cfg_train)
+@RunIf(min_gpus=1)
+@pytest.mark.slow
+def test_train_steps_gpu_amp(cfg_train: DictConfig) -> None:
+    """Train 2 steps on GPU with mixed-precision.
 
-# NOTE: this test is skipped for the sake of CI speed
+    :param cfg_train: A DictConfig containing a valid training configuration.
+    """
+    pytest.skip(
+        "This test is skipped for the sake of CI speed as well as the current lack of mixed-precision support."
+    )
+    HydraConfig().set_config(cfg_train)
+    with open_dict(cfg_train):
+        cfg_train.trainer.max_steps = 2
+        cfg_train.trainer.accelerator = "gpu"
+        cfg_train.trainer.precision = 16
+    train(cfg_train)
 
-# @pytest.mark.slow
-# def test_train_ddp_sim(cfg_train: DictConfig) -> None:
-#     """Simulate DDP (Distributed Data Parallel) for 2 steps on 2 CPU processes.
 
-#     :param cfg_train: A DictConfig containing a valid training configuration.
-#     """
-#     HydraConfig().set_config(cfg_train)
-#     with open_dict(cfg_train):
-#         cfg_train.trainer.max_steps = 2
-#         cfg_train.trainer.accelerator = "cpu"
-#         cfg_train.trainer.devices = 2
-#         cfg_train.trainer.strategy = "ddp_spawn"
-#     train(cfg_train)
+@pytest.mark.slow
+def test_train_steps_double_val_loop(cfg_train: DictConfig) -> None:
+    """Train 2 steps with validation loop twice per epoch.
+
+    :param cfg_train: A DictConfig containing a valid training configuration.
+    """
+    pytest.skip("This test is skipped for the sake of CI speed.")
+    HydraConfig().set_config(cfg_train)
+    with open_dict(cfg_train):
+        cfg_train.trainer.max_steps = 2
+        cfg_train.trainer.val_check_interval = 0.5
+    train(cfg_train)
+
+
+@pytest.mark.slow
+def test_train_ddp_sim(cfg_train: DictConfig) -> None:
+    """Simulate DDP (Distributed Data Parallel) for 2 steps on 2 CPU processes.
+
+    :param cfg_train: A DictConfig containing a valid training configuration.
+    """
+    pytest.skip("This test is skipped for the sake of CI speed.")
+    HydraConfig().set_config(cfg_train)
+    with open_dict(cfg_train):
+        cfg_train.trainer.max_steps = 2
+        cfg_train.trainer.accelerator = "cpu"
+        cfg_train.trainer.devices = 2
+        cfg_train.trainer.strategy = "ddp_spawn"
+    train(cfg_train)
 
 
 @pytest.mark.slow
