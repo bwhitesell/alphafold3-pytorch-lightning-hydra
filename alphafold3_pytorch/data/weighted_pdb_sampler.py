1c0ccdef3cbf5b8200855d173e16484854319d27
Alex Morehead <alex.morehead@gmail.com>
2024-07-19 19:02:23 -0500

    Distinguish types of clusters during weighted sampling

--- alphafold3_pytorch/data/weighted_pdb_sampler.py
+++ alphafold3_pytorch/data/weighted_pdb_sampler.py
@@ -1,12 +1,16 @@
 import os
-from typing import Iterator, Union
+from typing import Dict, Iterator, List, Tuple, Union
 
 import numpy as np
 import polars as pl
 from torch.utils.data import Sampler
 
+from alphafold3_pytorch.utils.tensor_typing import typecheck
+from scripts.cluster_pdb_mmcifs import CLUSTERING_MOLECULE_TYPE
 
-def get_chain_count(molecule_type) -> tuple[int, int, int]:
+
+@typecheck
+def get_chain_count(molecule_type: CLUSTERING_MOLECULE_TYPE) -> Tuple[int, int, int]:
     """
     Returns the number of protein, nucleic acid, and ligand chains in a
     molecule based on its type.
@@ -27,23 +31,44 @@ def get_chain_count(molecule_type) -> tuple[int, int, int]:
             raise ValueError(f"Unknown molecule type: {molecule_type}")
 
 
-def calculate_weight(alphas, beta, n_prot, n_nuc, n_ligand, cluster_size) -> float:
+@typecheck
+def calculate_weight(
+    alphas: Dict[str, float],
+    beta: float,
+    n_prot: int,
+    n_nuc: int,
+    n_ligand: int,
+    cluster_size: int,
+) -> float:
     """
     Calculates the weight of a chain or an interface according to the formula
-    provided in Section 2.5.1 of the AlphaFold3 supplementary information.
+    provided in Section 2.5.1 of the AlphaFold 3 supplementary materials.
     """
     return (beta / cluster_size) * (
         alphas["prot"] * n_prot + alphas["nuc"] * n_nuc + alphas["ligand"] * n_ligand
     )
 
 
-def get_chain_weight(molecule_type, cluster_size, alphas, beta) -> float:
+@typecheck
+def get_chain_weight(
+    molecule_type: CLUSTERING_MOLECULE_TYPE,
+    cluster_size: int,
+    alphas: Dict[str, float],
+    beta: float,
+) -> float:
     """Calculates the weight of a chain based on its type."""
     n_prot, n_nuc, n_ligand = get_chain_count(molecule_type)
     return calculate_weight(alphas, beta, n_prot, n_nuc, n_ligand, cluster_size)
 
 
-def get_interface_weight(molecule_type_1, molecule_type_2, cluster_size, alphas, beta) -> float:
+@typecheck
+def get_interface_weight(
+    molecule_type_1: CLUSTERING_MOLECULE_TYPE,
+    molecule_type_2: CLUSTERING_MOLECULE_TYPE,
+    cluster_size: int,
+    alphas: Dict[str, float],
+    beta: float,
+) -> float:
     """Calculates the weight of an interface based on the types of the two molecules."""
     p1, n1, l1 = get_chain_count(molecule_type_1)
     p2, n2, l2 = get_chain_count(molecule_type_2)
@@ -55,7 +80,11 @@ def get_interface_weight(molecule_type_1, molecule_type_2, cluster_size, alphas,
     return calculate_weight(alphas, beta, n_prot, n_nuc, n_ligand, cluster_size)
 
 
-def get_cluster_sizes(mapping, cluster_id_col) -> dict[int, int]:
+@typecheck
+def get_cluster_sizes(
+    mapping: pl.DataFrame,
+    cluster_id_col: str,
+) -> Dict[str, int]:
     """
     Returns a dictionary where keys are cluster IDs and values are the number
     of chains/interfaces in the cluster.
@@ -64,7 +93,10 @@ def get_cluster_sizes(mapping, cluster_id_col) -> dict[int, int]:
     return {row[0]: row[1] for row in cluster_sizes.iter_rows()}
 
 
-def compute_chain_weights(chains: pl.DataFrame, alphas, beta) -> pl.Series:
+@typecheck
+def compute_chain_weights(
+    chains: pl.DataFrame, alphas: Dict[str, float], beta: float
+) -> pl.Series:
     """Computes the weights of the chains based on the cluster sizes."""
     molecule_idx = chains.get_column_index("molecule_id")
     cluster_idx = chains.get_column_index("cluster_id")
@@ -85,7 +117,10 @@ def compute_chain_weights(chains: pl.DataFrame, alphas, beta) -> pl.Series:
     )
 
 
-def compute_interface_weights(interfaces: pl.DataFrame, alphas, beta) -> pl.Series:
+@typecheck
+def compute_interface_weights(
+    interfaces: pl.DataFrame, alphas: Dict[str, float], beta: float
+) -> pl.Series:
     """Computes the weights of the interfaces based on the chain weights."""
     molecule_idx_1 = interfaces.get_column_index("interface_molecule_id_1")
     molecule_idx_2 = interfaces.get_column_index("interface_molecule_id_2")
@@ -108,9 +143,9 @@ def compute_interface_weights(interfaces: pl.DataFrame, alphas, beta) -> pl.Seri
     )
 
 
-class WeightedSamplerPDB(Sampler[list[str]]):
+class WeightedPDBSampler(Sampler[List[str]]):
     """
-    Initializes a dataset for weighted sampling of PDB IDs.
+    Initializes a sampler for weighted sampling of PDB and chain/interface IDs.
 
     :param chain_mapping_paths: Path to the CSV file containing chain cluster
         mappings. If multiple paths are provided, they will be concatenated.
@@ -137,7 +172,7 @@ class WeightedSamplerPDB(Sampler[list[str]]):
 
     def __init__(
         self,
-        chain_mapping_paths: Union[str, list[str]],
+        chain_mapping_paths: Union[str, List[str]],
         interface_mapping_path: str,
         batch_size: int,
         beta_chain: float = 0.5,
@@ -145,15 +180,32 @@ class WeightedSamplerPDB(Sampler[list[str]]):
         alpha_prot: float = 3.0,
         alpha_nuc: float = 3.0,
         alpha_ligand: float = 1.0,
-        pdb_ids_to_skip: list[str] = [],
+        pdb_ids_to_skip: List[str] = [],
     ):
         # Load chain and interface mappings
         if not isinstance(chain_mapping_paths, list):
             chain_mapping_paths = [chain_mapping_paths]
 
-        chain_mapping = [pl.read_csv(path) for path in chain_mapping_paths]
+        chain_mapping = []
+        for path in chain_mapping_paths:
+            mapping = pl.read_csv(path)
+            mapping = mapping.with_columns(
+                (pl.col("molecule_id") + "-" + pl.col("cluster_id").cast(pl.String)).alias(
+                    "cluster_id"
+                )
+            )
+            chain_mapping.append(mapping)
         chain_mapping = pl.concat(chain_mapping)
         interface_mapping = pl.read_csv(interface_mapping_path)
+        interface_mapping = interface_mapping.with_columns(
+            (
+                pl.col("interface_molecule_id_1")
+                + "-"
+                + pl.col("interface_molecule_id_2")
+                + "-"
+                + pl.col("interface_cluster_id").cast(pl.String)
+            ).alias("interface_cluster_id")
+        )
 
         # Filter out unwanted PDB IDs
         if len(pdb_ids_to_skip) > 0:
@@ -177,13 +229,26 @@ class WeightedSamplerPDB(Sampler[list[str]]):
         )
 
         # Concatenate chain and interface mappings
-        chain_mapping = chain_mapping.select(["pdb_id", "cluster_id", "weight"])
-
-        num_chain_clusters = chain_mapping.get_column("cluster_id").max() + 1
-        interface_mapping = interface_mapping.with_columns(
-            (pl.col("interface_cluster_id") + num_chain_clusters).alias("cluster_id")
+        chain_mapping = chain_mapping.with_columns(
+            [
+                pl.col("chain_id").alias("chain_id_1"),
+                pl.lit("").alias("chain_id_2"),
+            ]
         )
-        interface_mapping = interface_mapping.select(["pdb_id", "cluster_id", "weight"])
+        chain_mapping = chain_mapping.select(
+            ["pdb_id", "chain_id_1", "chain_id_2", "cluster_id", "weight"]
+        )
+
+        interface_mapping = interface_mapping.with_columns(
+            [
+                pl.col("interface_chain_id_1").alias("chain_id_1"),
+                pl.col("interface_chain_id_2").alias("chain_id_2"),
+                pl.col("interface_cluster_id").alias("cluster_id"),
+            ]
+        )
+        interface_mapping = interface_mapping.select(
+            ["pdb_id", "chain_id_1", "chain_id_2", "cluster_id", "weight"]
+        )
         self.mappings = chain_mapping.extend(interface_mapping)
 
         # Normalize weights
@@ -194,38 +259,44 @@ class WeightedSamplerPDB(Sampler[list[str]]):
         """Returns the number of batches in the dataset."""
         return len(self.mappings) // self.batch_size
 
-    def __iter__(self) -> Iterator[list[str]]:
+    def __iter__(self) -> Iterator[List[str]]:
         """Returns an iterator that yields batches of PDB IDs."""
         while True:
             yield self.sample(self.batch_size)
 
-    def sample(self, batch_size: int) -> list[str]:
-        """Samples PDB IDs based on the weights of the chains/interfaces."""
+    @typecheck
+    def sample(self, batch_size: int) -> List[Tuple[str, str, str]]:
+        """Samples a chain ID or interface ID based on the weights of the chains/interfaces."""
         indices = np.random.choice(len(self.mappings), size=batch_size, p=self.weights)
-        return self.mappings.get_column("pdb_id").gather(indices).to_list()
+        return self.mappings[indices].select(["pdb_id", "chain_id_1", "chain_id_2"]).rows()
 
-    def cluster_based_sample(self, batch_size: int) -> list[str]:
+    @typecheck
+    def cluster_based_sample(self, batch_size: int) -> List[Tuple[str, str, str]]:
         """
-        Samples PDB IDs based on cluster IDs. For each batch, a number of cluster IDs
-        are selected randomly, and a PDB ID is sampled from each cluster based on the
+        Samples a chain ID or interface ID based on cluster IDs. For each batch, a number of cluster IDs
+        are selected randomly, and a chain ID or interface ID is sampled from each cluster based on the
         weights of the chains/interfaces in the cluster.
 
         Warning! Significantly slower than the regular `sample` method.
         """
         cluster_ids = self.mappings.get_column("cluster_id").unique().sample(batch_size)
 
-        pdb_ids = []
+        pdb_chain_ids = []
         for cluster_id in cluster_ids:
             cluster = self.mappings.filter(pl.col("cluster_id") == cluster_id)
             if len(cluster) == 1:
-                pdb_ids.append(cluster.item(0, "pdb_id"))
+                pdb_chain_ids.append(
+                    cluster[0].select(["pdb_id", "chain_id_1", "chain_id_2"]).row(0)
+                )
                 continue
             cluster_weights = cluster.get_column("weight").to_numpy()
             cluster_weights = cluster_weights / cluster_weights.sum()
             idx = np.random.choice(len(cluster), p=cluster_weights)
-            pdb_ids.append(cluster.item(idx, "pdb_id"))
+            pdb_chain_ids.append(
+                cluster[idx].select(["pdb_id", "chain_id_1", "chain_id_2"]).row(0)
+            )
 
-        return pdb_ids
+        return pdb_chain_ids
 
 
 if __name__ == "__main__":
@@ -251,10 +322,11 @@ if __name__ == "__main__":
         ),
     ]
 
-    dataset = WeightedSamplerPDB(
+    sampler = WeightedPDBSampler(
         chain_mapping_paths=chain_mapping_paths,
         interface_mapping_path=interface_mapping_path,
         batch_size=64,
     )
 
-    print(dataset.sample(64))
+    print(sampler.sample(64))
+    print(sampler.cluster_based_sample(64))
