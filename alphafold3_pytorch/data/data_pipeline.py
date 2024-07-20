fde16924f074474e9d9936ca2801783ac7781c36
Alex Morehead <acmwhb@missouri.edu>
2024-07-20 14:14:39 -0500

    Create outline of chain cropping

--- alphafold3_pytorch/data/data_pipeline.py
+++ alphafold3_pytorch/data/data_pipeline.py
@@ -151,12 +151,15 @@ if __name__ == "__main__":
         file_id=file_id,
     )
     mmcif_feats, assembly = make_mmcif_features(mmcif_object)
+    # cropped_assembly = assembly.contiguous_crop(384)
     mmcif_string = to_mmcif(
         assembly,
+        # cropped_assembly,
         file_id=file_id,
         gapless_poly_seq=True,
         insert_alphafold_mmcif_metadata=False,
         unique_res_atom_names=assembly.unique_res_atom_names,
+        # unique_res_atom_names=cropped_assembly.unique_res_atom_names,
     )
     with open(os.path.basename(filepath).replace(".cif", "_reconstructed.cif"), "w") as f:
         f.write(mmcif_string)
