f390751ccebda8299dd5a9dedc478284de80d9ba
Alex Morehead <acmwhb@missouri.edu>
2024-08-08 20:43:21 -0500

    Fix var

--- alphafold3_pytorch/models/components/alphafold3.py
+++ alphafold3_pytorch/models/components/alphafold3.py
@@ -4687,7 +4687,7 @@ class ComputeModelSelectionScore(Module):
             if (atom_asym_id[b] == -1).all():
                 # TODO: Remove this check after fixing the batching bug in `batch_repeat_interleave()`
                 # (see https://github.com/lucidrains/alphafold3-pytorch/issues/158)
-                log.warning(
+                logger.warning(
                     f"Found erroneous `atom_asym_id` element at index {b}. Returning null lDDT for this batch element."
                 )
                 weighted_lddt[b] = torch.tensor(1e-6, device=device)
