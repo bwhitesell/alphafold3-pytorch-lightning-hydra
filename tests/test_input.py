0ae93b9c3e4a9992d32fc2095bf138984cccbcea
Alex Morehead <acmwhb@missouri.edu>
2024-08-20 16:38:23 -0500

    Reduce scale

--- tests/test_input.py
+++ tests/test_input.py
@@ -190,10 +190,7 @@ def test_pdbinput_input():
     """Test the PDBInput class, particularly its input transformations for mmCIF files."""
     filepath = os.path.join("data", "test", "7a4d-assembly1.cif")
     file_id = os.path.splitext(os.path.basename(filepath))[0]
-    assert os.path.exists(filepath)
-
-    if os.path.exists(filepath.replace(".cif", "-sampled.cif")):
-        os.remove(filepath.replace(".cif", "-sampled.cif"))
+    assert os.path.exists(filepath), f"File {filepath} does not exist."
 
     train_pdb_input = PDBInput(
         filepath,
@@ -214,12 +211,12 @@ def test_pdbinput_input():
     # training
 
     alphafold3 = Alphafold3(
-        dim_atom=8,
-        dim_atompair=8,
+        dim_atom=2,
+        dim_atompair=2,
         dim_input_embedder_token=8,
-        dim_single=8,
-        dim_pairwise=8,
-        dim_token=8,
+        dim_single=2,
+        dim_pairwise=2,
+        dim_token=2,
         dim_atom_inputs=3,
         dim_atompair_inputs=5,
         atoms_per_window=27,
@@ -227,20 +224,21 @@ def test_pdbinput_input():
         num_molecule_mods=4,
         num_dist_bins=38,
         num_rollout_steps=2,
+        diffusion_num_augmentations=2,
         confidence_head_kwargs=dict(pairformer_depth=1),
         template_embedder_kwargs=dict(pairformer_stack_depth=1),
-        msa_module_kwargs=dict(depth=1),
+        msa_module_kwargs=dict(depth=1, dim_msa=2),
         pairformer_stack=dict(
             depth=1,
-            pair_bias_attn_dim_head=4,
+            pair_bias_attn_dim_head=1,
             pair_bias_attn_heads=2,
         ),
         diffusion_module_kwargs=dict(
             atom_encoder_depth=1,
             token_transformer_depth=1,
             atom_decoder_depth=1,
-            atom_decoder_kwargs=dict(attn_pair_bias_kwargs=dict(dim_head=4)),
-            atom_encoder_kwargs=dict(attn_pair_bias_kwargs=dict(dim_head=4)),
+            atom_decoder_kwargs=dict(attn_pair_bias_kwargs=dict(dim_head=1)),
+            atom_encoder_kwargs=dict(attn_pair_bias_kwargs=dict(dim_head=1)),
         ),
     )
 
