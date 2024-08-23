3598a8c1afb91562e90e92e4f3f7e462fa053fa0
Alex Morehead <acmwhb@missouri.edu>
2024-08-23 16:40:28 -0500

    Update run resuming logic

--- scripts/slurm/af3_overfitting_experiment_e1_bs1_large.sh
+++ scripts/slurm/af3_overfitting_experiment_e1_bs1_large.sh
@@ -6,11 +6,11 @@
 #SBATCH --nodes=1                                             # NOTE: this needs to match Lightning's `Trainer(num_nodes=...)`
 #SBATCH --gres=gpu:1                                          # NOTE: requests any GPU resource(s)
 #SBATCH --ntasks-per-node=1                                   # NOTE: this needs to be `1` on SLURM clusters when using Lightning's `ddp_spawn` strategy`; otherwise, set to match Lightning's quantity of `Trainer(devices=...)`
-#SBATCH --mem=59G                                             # NOTE: use `--mem=0` to request all memory "available" on the assigned node
-#SBATCH --time 2-00:00:00                                     # time limit for the job (up to 2 days: `2-00:00:00`)
+#SBATCH --time 0-24:00:00                                     # time limit for the job (up to 24 hours: `0-24:00:00`)
 #SBATCH --job-name=af3_overfitting_e1_bs1_large               # job name
 #SBATCH --output=J-%x.%j.out                                  # output log file
 #SBATCH --error=J-%x.%j.err                                   # error log file
+#SBATCH --signal=SIGUSR1@90                                   # send SIGUSR1 90 seconds before job end to trigger job resubmission
 # NOTE: One cannot request exclusive access to a node
 #################################################################
 
@@ -27,14 +27,17 @@ rm -rf ${MIOPEN_USER_DB_PATH}
 mkdir -p ${MIOPEN_USER_DB_PATH}
 export containerImage=/scratch/pawsey1018/amorehead/af3-pytorch-lightning-hydra/af3-pytorch-lightning-hydra_0.3.8_dev.sif
 
+# Set up WandB run
+RUN_ID="fidch6u7"  # NOTE: Generate a unique ID for each run using `python3 scripts/generate_id.py`
+
 # Run container
-singularity exec --rocm \
+srun singularity exec --rocm \
     --cleanenv \
     -H "$PWD":/home \
     -B alphafold3-pytorch-lightning-hydra:/alphafold3-pytorch-lightning-hydra \
     --pwd /alphafold3-pytorch-lightning-hydra \
     "$containerImage" \
-    python3 alphafold3_pytorch/train.py experiment=af3_overfitting_e1_bs1_large
+    WANDB_RESUME=allow WANDB_RUN_ID="$RUN_ID" python3 alphafold3_pytorch/train.py experiment=af3_overfitting_e1_bs1_large
 
 # Inform user of run completion
 echo "Run completed for job: $SLURM_JOB_NAME."
