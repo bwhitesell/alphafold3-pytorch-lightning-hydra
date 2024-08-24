6296e44ada4cb9135b32e698b7a73915e5cc19fb
Alex Morehead <acmwhb@missouri.edu>
2024-08-23 20:49:32 -0500

    Install WandB during runs

--- scripts/slurm/af3_overfitting_experiment_e1_bs1_large.sh
+++ scripts/slurm/af3_overfitting_experiment_e1_bs1_large.sh
@@ -38,7 +38,7 @@ srun singularity exec --rocm \
     -B alphafold3-pytorch-lightning-hydra:/alphafold3-pytorch-lightning-hydra \
     --pwd /alphafold3-pytorch-lightning-hydra \
     "$containerImage" \
-    python3 alphafold3_pytorch/train.py experiment=af3_overfitting_e1_bs1_large
+    pip3 install wandb && python3 alphafold3_pytorch/train.py experiment=af3_overfitting_e1_bs1_large
 
 # Inform user of run completion
 echo "Run completed for job: $SLURM_JOB_NAME"
