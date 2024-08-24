0b2b2fd360d718cd621ce35d63c749b89832621e
Alex Morehead <acmwhb@missouri.edu>
2024-08-24 14:49:26 -0500

    Simplify runs

--- scripts/slurm/af3_overfitting_experiment_e1_bs1.sh
+++ scripts/slurm/af3_overfitting_experiment_e1_bs1.sh
@@ -4,10 +4,10 @@
 #SBATCH --partition=gpu                                       # use partition `gpu` for GPU nodes
 #SBATCH --account=pawsey1018-gpu                              # IMPORTANT: use your own project and the -gpu suffix
 #SBATCH --nodes=1                                             # NOTE: this needs to match Lightning's `Trainer(num_nodes=...)`
-#SBATCH --gres=gpu:3                                          # NOTE: requests any GPU resource(s)
-#SBATCH --ntasks-per-node=3                                   # NOTE: this needs to be `1` on SLURM clusters when using Lightning's `ddp_spawn` strategy`; otherwise, set to match Lightning's quantity of `Trainer(devices=...)`
+#SBATCH --gres=gpu:1                                          # NOTE: requests any GPU resource(s)
+#SBATCH --ntasks-per-node=1                                   # NOTE: this needs to be `1` on SLURM clusters when using Lightning's `ddp_spawn` strategy`; otherwise, set to match Lightning's quantity of `Trainer(devices=...)`
 #SBATCH --time 0-24:00:00                                     # time limit for the job (up to 24 hours: `0-24:00:00`)
-#SBATCH --job-name=af3_overfitting_e3_bs1_large               # job name
+#SBATCH --job-name=af3_overfitting_e1_bs1                     # job name
 #SBATCH --output=J-%x.%j.out                                  # output log file
 #SBATCH --error=J-%x.%j.err                                   # error log file
 #SBATCH --signal=SIGUSR1@90                                   # send SIGUSR1 90 seconds before job end to trigger job resubmission
@@ -28,7 +28,7 @@ mkdir -p "${MIOPEN_USER_DB_PATH}"
 export containerImage="/scratch/pawsey1018/$USER/af3-pytorch-lightning-hydra/af3-pytorch-lightning-hydra_0.4.5_dev.sif"
 
 # Set up WandB run
-RUN_ID="6nattsz0"  # NOTE: Generate a unique ID for each run using `python3 scripts/generate_id.py`
+RUN_ID="b9ego5ct"  # NOTE: Generate a unique ID for each run using `python3 scripts/generate_id.py`
 
 # Run container
 srun singularity exec --rocm \
@@ -42,9 +42,10 @@ srun singularity exec --rocm \
         cd /alphafold3-pytorch-lightning-hydra && \
         WANDB_RESUME=allow WANDB_RUN_ID=$RUN_ID \
         python3 alphafold3_pytorch/train.py
-        experiment=af3_overfitting_e3_bs1_large \
+        experiment=af3_overfitting_e1_bs1 \
+        trainer.strategy=default \
         trainer.num_nodes=1 \
-        trainer.devices=3
+        trainer.devices=1
     "
 
 # Inform user of run completion
