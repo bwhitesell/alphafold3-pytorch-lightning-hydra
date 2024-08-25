c9073fd4a20339ba6c7d2fca979530994f7e7f15
Alex Morehead <acmwhb@missouri.edu>
2024-08-24 20:19:05 -0500

    Test out smaller FSDP run

--- scripts/slurm/af3_overfitting_experiment_e2_bs1.sh
+++ scripts/slurm/af3_overfitting_experiment_e2_bs1.sh
@@ -4,8 +4,8 @@
 #SBATCH --partition=gpu                                       # use partition `gpu` for GPU nodes
 #SBATCH --account=pawsey1018-gpu                              # IMPORTANT: use your own project and the -gpu suffix
 #SBATCH --nodes=1                                             # NOTE: this needs to match Lightning's `Trainer(num_nodes=...)`
-#SBATCH --gres=gpu:1                                          # NOTE: requests any GPU resource(s)
-#SBATCH --ntasks-per-node=1                                   # NOTE: this needs to be `1` on SLURM clusters when using Lightning's `ddp_spawn` strategy`; otherwise, set to match Lightning's quantity of `Trainer(devices=...)`
+#SBATCH --gres=gpu:2                                          # NOTE: requests any GPU resource(s)
+#SBATCH --ntasks-per-node=2                                   # NOTE: this needs to be `1` on SLURM clusters when using Lightning's `ddp_spawn` strategy`; otherwise, set to match Lightning's quantity of `Trainer(devices=...)`
 #SBATCH --time 0-24:00:00                                     # time limit for the job (up to 24 hours: `0-24:00:00`)
 #SBATCH --job-name=af3_overfitting_e2_bs1                     # job name
 #SBATCH --output=J-%x.%j.out                                  # output log file
@@ -43,9 +43,9 @@ srun singularity exec --rocm \
         WANDB_RESUME=allow WANDB_RUN_ID=$RUN_ID \
         python3 alphafold3_pytorch/train.py \
         experiment=af3_overfitting_e2_bs1 \
-        trainer=default \
+        data.batch_size=2 \
         trainer.num_nodes=1 \
-        trainer.devices=1
+        trainer.devices=2
     "
 
 # Inform user of run completion
