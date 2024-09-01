3d504669078fd531b4a3ef9626851aee62f8a5fd
Alex Morehead <acmwhb@missouri.edu>
2024-09-01 15:11:00 -0500

    Switch to default trainer for e1

--- scripts/slurm/af3_overfitting_experiment_e1_bs1.sh
+++ scripts/slurm/af3_overfitting_experiment_e1_bs1.sh
@@ -9,7 +9,6 @@
 #SBATCH --job-name=af3_overfitting_e1_bs1                     # job name
 #SBATCH --output=J-%x.%j.out                                  # output log file
 #SBATCH --error=J-%x.%j.err                                   # error log file
-#SBATCH --exclusive                                           # request exclusive node access
 #SBATCH --signal=SIGUSR1@90                                   # send SIGUSR1 90 seconds before job end to trigger job resubmission
 #################################################################
 
@@ -28,26 +27,14 @@ mkdir -p "${MIOPEN_USER_DB_PATH}"
 # Define the container image path
 export SINGULARITY_CONTAINER="/scratch/pawsey1018/$USER/af3-pytorch-lightning-hydra/af3-pytorch-lightning-hydra_0.4.20_dev.sif"
 
-# Set number of PyTorch (GPU) processes per node to be spawned by torchrun - NOTE: One for each GCD
-NUM_PYTORCH_PROCESSES=1
 # Set the number of threads to be generated for each PyTorch (GPU) process
 export OMP_NUM_THREADS=8
-
-# Define the compute node executing the batch script
-RDZV_HOST=$(hostname)
-export RDZV_HOST
-export RDZV_PORT=29400
-
-# NOTE: The following `srun` command gives all the available resources to
-# `torchrun` which will then distribute them internally to the processes
-# it creates. Importantly, notice that processes are NOT created by srun!
-# For what `srun` is concerned, only one task is created, the `torchrun` process.
 
 # Define WandB run ID
 RUN_ID="8qnn9fqs"  # NOTE: Generate a unique ID for each run using `python3 scripts/generate_id.py`
 
 # Run Singularity container
-srun -c 64 singularity exec \
+srun -c 64 singularity exec --rocm \
     --cleanenv \
     -H "$PWD":/home \
     -B alphafold3-pytorch-lightning-hydra:/alphafold3-pytorch-lightning-hydra \
@@ -55,17 +42,11 @@ srun -c 64 singularity exec \
     "$SINGULARITY_CONTAINER" \
     bash -c "
         WANDB_RESUME=allow WANDB_RUN_ID=$RUN_ID CONDA_PREFIX=/opt/miniforge3 \
-        torchrun \
-        --nnodes=$SLURM_JOB_NUM_NODES \
-        --nproc_per_node=$NUM_PYTORCH_PROCESSES \
-        --rdzv_id=$SLURM_JOB_ID \
-        --rdzv_backend=c10d \
-        --rdzv_endpoint=$RDZV_HOST:$RDZV_PORT \
-        alphafold3_pytorch/train.py \
+        python3 alphafold3_pytorch/train.py \
         experiment=af3_overfitting_e1_bs1 \
-        data.batch_size=$NUM_PYTORCH_PROCESSES \
-        trainer.num_nodes=$SLURM_JOB_NUM_NODES \
-        trainer.devices='[0]'
+        data.batch_size=1 \
+        trainer.num_nodes=1 \
+        trainer.devices=1
     "
 
 # Inform user of run completion
