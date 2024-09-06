f57df80999c3e48355ae8e7f88b99bb243f4aa83
Alex Morehead <acmwhb@missouri.edu>
2024-09-06 17:00:38 -0500

    Try DS stage 3 to reduce total CPU usage

--- scripts/slurm/af3_initial_training.sh
+++ scripts/slurm/af3_initial_training.sh
@@ -44,7 +44,7 @@ export RDZV_PORT=29400
 # For what `srun` is concerned, only one task is created, the `torchrun` process.
 
 # Define WandB run ID
-RUN_ID="w9yeufz2"  # NOTE: Generate a unique ID for each run using `python3 scripts/generate_id.py`
+RUN_ID="qv9qz90h"  # NOTE: Generate a unique ID for each run using `python3 scripts/generate_id.py`
 
 # Run Singularity container
 srun -c 64 singularity exec \
