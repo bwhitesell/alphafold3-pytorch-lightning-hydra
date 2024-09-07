0157de6faedc1e0ed541a1cc5263e57573cdd8bd
Alex Morehead <acmwhb@missouri.edu>
2024-09-07 11:07:24 -0500

    Fix syntax

--- scripts/slurm/af3_initial_training.sh
+++ scripts/slurm/af3_initial_training.sh
@@ -54,7 +54,7 @@ srun -c 64 singularity exec \
     --pwd /alphafold3-pytorch-lightning-hydra \
     "$SINGULARITY_CONTAINER" \
     bash -c "
-        PATH=/root/.cargo/bin:$PATH python3 -m pip install git+https://github.com/wandb/wandb.git@41c9cb0cee6f763931045c25bf2fbdba32699575
+        PATH=/root/.cargo/bin:$PATH python3 -m pip install git+https://github.com/wandb/wandb.git@41c9cb0cee6f763931045c25bf2fbdba32699575 \
         && cd /alphafold3-pytorch-lightning-hydra \
         && WANDB_RESUME=allow WANDB_RUN_ID=$RUN_ID OMP_NUM_THREADS=$OMP_NUM_THREADS \
         torchrun \
