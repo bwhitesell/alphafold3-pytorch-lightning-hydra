5740fc2324ebee961371ddf2e06fc261d6d7c245
Alex Morehead <acmwhb@missouri.edu>
2024-09-09 10:37:16 -0500

    Sync with main

--- tests/test_dataloading.py
+++ tests/test_dataloading.py
@@ -1,4 +1,4 @@
-import os
+from pathlib import Path
 
 import pytest
 
@@ -14,24 +14,19 @@ from alphafold3_pytorch.models.components.inputs import (
 
 def test_data_input():
     """Test a PDBDataset constructed using a WeightedPDBSampler."""
-    data_test = os.path.join("data", "test")
-    data_test_mmcif_dir = os.path.join(data_test, "mmcif")
-    data_test_clusterings_dir = os.path.join(data_test, "data_caches", "clusterings")
+    data_test = Path("data", "test")
+    data_test_mmcif_dir = data_test / "mmcifs"
+    data_test_clusterings_dir = data_test / "data_caches" / "clusterings"
 
-    if not os.path.exists(data_test_mmcif_dir):
+    if not data_test_mmcif_dir.exists():
         pytest.skip(f"The directory `{data_test_mmcif_dir}` is not populated yet.")
 
-    interface_mapping_path = os.path.join(
-        data_test_clusterings_dir, "interface_cluster_mapping.csv"
-    )
+    interface_mapping_path = str(data_test_clusterings_dir / "interface_cluster_mapping.csv")
     chain_mapping_paths = [
-        os.path.join(data_test_clusterings_dir, "ligand_chain_cluster_mapping.csv"),
-        os.path.join(
-            data_test_clusterings_dir,
-            "nucleic_acid_chain_cluster_mapping.csv",
-        ),
-        os.path.join(data_test_clusterings_dir, "peptide_chain_cluster_mapping.csv"),
-        os.path.join(data_test_clusterings_dir, "protein_chain_cluster_mapping.csv"),
+        str(data_test_clusterings_dir / "ligand_chain_cluster_mapping.csv"),
+        str(data_test_clusterings_dir / "nucleic_acid_chain_cluster_mapping.csv"),
+        str(data_test_clusterings_dir / "peptide_chain_cluster_mapping.csv"),
+        str(data_test_clusterings_dir / "protein_chain_cluster_mapping.csv"),
     ]
 
     sampler = WeightedPDBSampler(
@@ -40,8 +35,19 @@ def test_data_input():
         batch_size=64,
     )
 
+    sampler_pdb_ids = set(sampler.mappings.get_column("pdb_id").to_list())
+    test_ids = set(
+        filepath.stem
+        for filepath in data_test_mmcif_dir.glob("**/*.cif")
+        if filepath.stem in sampler_pdb_ids
+    )
+
     dataset = PDBDataset(
-        folder=data_test_mmcif_dir, sampler=sampler, sample_type="default", crop_size=128
+        folder=data_test_mmcif_dir,
+        sampler=sampler,
+        sample_type="default",
+        sample_only_pdb_ids=test_ids,
+        crop_size=128,
     )
 
     mol_input = pdb_input_to_molecule_input(pdb_input=dataset[0])
