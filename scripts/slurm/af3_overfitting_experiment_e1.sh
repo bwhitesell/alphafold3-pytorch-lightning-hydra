d3c15db89247adafd1ff7923b55820ff8da175e3
Alex Morehead <acmwhb@missouri.edu>
2024-09-12 14:04:16 -0500

    Set OMP_NUM_THREADS back to 8

--- scripts/slurm/af3_overfitting_experiment_e1.sh
+++ scripts/slurm/af3_overfitting_experiment_e1.sh
@@ -28,7 +28,7 @@ mkdir -p "${MIOPEN_USER_DB_PATH}"
 export SINGULARITY_CONTAINER="/scratch/pawsey1018/$USER/af3-pytorch-lightning-hydra/af3-pytorch-lightning-hydra_0.4.50_dev.sif"
 
 # Set the number of threads to be generated for each PyTorch (GPU) process
-export OMP_NUM_THREADS=1
+export OMP_NUM_THREADS=8
 
 # Define WandB run ID
 RUN_ID="kah0gjep"  # NOTE: Generate a unique ID for each run using `python3 scripts/generate_id.py`
