25c775245b92d56b3bcac72f84f0882f8e83ea73
Alex Morehead <acmwhb@missouri.edu>
2024-09-12 10:24:19 -0500

    Update ID

--- scripts/slurm/af3_initial_training.sh
+++ scripts/slurm/af3_initial_training.sh
@@ -47,7 +47,7 @@ export PYTHONFAULTHANDLER=1
 # For what `srun` is concerned, only one task is created, the `torchrun` process.
 
 # Define WandB run ID
-RUN_ID="xvr2i553"  # NOTE: Generate a unique ID for each run using `python3 scripts/generate_id.py`
+RUN_ID="fczsh5xu"  # NOTE: Generate a unique ID for each run using `python3 scripts/generate_id.py`
 
 # Run Singularity container
 srun -c 64 singularity exec \
