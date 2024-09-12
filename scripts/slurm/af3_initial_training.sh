ef53d9ac663160c50384a148b84ab0b61b7665b8
Alex Morehead <acmwhb@missouri.edu>
2024-09-12 13:01:54 -0500

    Disable P2P again

--- scripts/slurm/af3_initial_training.sh
+++ scripts/slurm/af3_initial_training.sh
@@ -37,13 +37,16 @@ RDZV_HOST=$(hostname)
 export RDZV_HOST
 export RDZV_PORT=29400
 
+# Configure NCCL to disable P2P communication
+export NCCL_P2P_DISABLE=1
+
 # NOTE: The following `srun` command gives all the available resources to
 # `torchrun` which will then distribute them internally to the processes
 # it creates. Importantly, notice that processes are NOT created by srun!
 # For what `srun` is concerned, only one task is created, the `torchrun` process.
 
 # Define WandB run ID
-RUN_ID="rpnm8ejj"  # NOTE: Generate a unique ID for each run using `python3 scripts/generate_id.py`
+RUN_ID="yxlmn9e1"  # NOTE: Generate a unique ID for each run using `python3 scripts/generate_id.py`
 
 # Run Singularity container
 srun -c 64 singularity exec \
@@ -55,7 +58,7 @@ srun -c 64 singularity exec \
     bash -c "
         /usr/bin/kalign --version \
         && python3 -c 'import torch; print(torch.__version__)' \
-        && WANDB_RESUME=allow WANDB_RUN_ID=$RUN_ID OMP_NUM_THREADS=$OMP_NUM_THREADS NCCL_DEBUG=INFO PYTHONFAULTHANDLER=1 \
+        && WANDB_RESUME=allow WANDB_RUN_ID=$RUN_ID OMP_NUM_THREADS=$OMP_NUM_THREADS NCCL_DEBUG=INFO PYTHONFAULTHANDLER=1 NCCL_P2P_DISABLE=1 \
         torchrun \
         --nnodes=$SLURM_JOB_NUM_NODES \
         --nproc_per_node=$NUM_PYTORCH_PROCESSES \
