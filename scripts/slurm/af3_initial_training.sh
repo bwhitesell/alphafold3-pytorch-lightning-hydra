82010dc4ea60eec9340937e2650a0793f2220e06
Alex Morehead <acmwhb@missouri.edu>
2024-09-11 20:50:42 -0500

    Fix MSA type

--- scripts/slurm/af3_initial_training.sh
+++ scripts/slurm/af3_initial_training.sh
@@ -47,7 +47,7 @@ export PYTHONFAULTHANDLER=1
 # For what `srun` is concerned, only one task is created, the `torchrun` process.
 
 # Define WandB run ID
-RUN_ID="0kus44tj"  # NOTE: Generate a unique ID for each run using `python3 scripts/generate_id.py`
+RUN_ID="hb9jhr4w"  # NOTE: Generate a unique ID for each run using `python3 scripts/generate_id.py`
 
 # Run Singularity container
 srun -c 64 singularity exec \
