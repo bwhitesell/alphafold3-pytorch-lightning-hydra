91028187bab5ca79c190450f8b23a02463590b35
Alex Morehead <acmwhb@missouri.edu>
2024-09-12 18:00:16 -0500

    ROCm 6.2 froze, go back to 5.7 and try 1 thread

--- scripts/slurm/af3_initial_training.sh
+++ scripts/slurm/af3_initial_training.sh
@@ -25,12 +25,12 @@ rm -rf "${MIOPEN_USER_DB_PATH}"
 mkdir -p "${MIOPEN_USER_DB_PATH}"
 
 # Define the container image path
-export SINGULARITY_CONTAINER="/scratch/pawsey1018/$USER/af3-pytorch-lightning-hydra/af3-pytorch-lightning-hydra_0.5.5_dev.sif"
+export SINGULARITY_CONTAINER="/scratch/pawsey1018/$USER/af3-pytorch-lightning-hydra/af3-pytorch-lightning-hydra_0.5.6_dev.sif"
 
 # Set number of PyTorch (GPU) processes per node to be spawned by torchrun - NOTE: One for each GCD
 NUM_PYTORCH_PROCESSES=8
 # Set the number of threads to be generated for each PyTorch (GPU) process
-export OMP_NUM_THREADS=8
+export OMP_NUM_THREADS=1
 
 # Define the compute node executing the batch script
 RDZV_HOST=$(hostname)
@@ -43,7 +43,7 @@ export RDZV_PORT=29400
 # For what `srun` is concerned, only one task is created, the `torchrun` process.
 
 # Define WandB run ID
-RUN_ID="12tvh2v0"  # NOTE: Generate a unique ID for each run using `python3 scripts/generate_id.py`
+RUN_ID="7dyebpy1"  # NOTE: Generate a unique ID for each run using `python3 scripts/generate_id.py`
 
 # Run Singularity container
 srun -c 64 singularity exec \
@@ -54,7 +54,7 @@ srun -c 64 singularity exec \
     "$SINGULARITY_CONTAINER" \
     bash -c "
         /usr/bin/kalign --version \
-        && WANDB_RESUME=allow WANDB_RUN_ID=$RUN_ID OMP_NUM_THREADS=$OMP_NUM_THREADS NCCL_DEBUG=INFO HIP_LAUNCH_BLOCKING=1 HSA_ENABLE_SDMA=0 HSA_XNACK=1 \
+        && WANDB_RESUME=allow WANDB_RUN_ID=$RUN_ID OMP_NUM_THREADS=$OMP_NUM_THREADS NCCL_DEBUG=INFO HIP_LAUNCH_BLOCKING=1 \
         torchrun \
         --nnodes=$SLURM_JOB_NUM_NODES \
         --nproc_per_node=$NUM_PYTORCH_PROCESSES \
