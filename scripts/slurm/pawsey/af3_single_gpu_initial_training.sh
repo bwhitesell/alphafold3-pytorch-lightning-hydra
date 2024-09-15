1ba17af663d3686557e34fe2b2a52d75be7f7147
Alex Morehead <acmwhb@missouri.edu>
2024-09-15 15:45:04 -0500

    Reduce memory

--- scripts/slurm/pawsey/af3_single_gpu_initial_training.sh
+++ scripts/slurm/pawsey/af3_single_gpu_initial_training.sh
@@ -23,7 +23,7 @@ export SINGULARITY_CONTAINER="/scratch/pawsey1018/$USER/af3-pytorch-lightning-hy
 export OMP_NUM_THREADS=8
 
 # Define WandB run ID
-RUN_ID="u7w6d5xc"  # NOTE: Generate a unique ID for each run using `python3 scripts/generate_id.py`
+RUN_ID="v7w6d5xc"  # NOTE: Generate a unique ID for each run using `python3 scripts/generate_id.py`
 
 # Run Singularity container
 srun singularity exec --rocm \
@@ -37,6 +37,8 @@ srun singularity exec --rocm \
         && WANDB_RESUME=allow WANDB_RUN_ID=$RUN_ID OMP_NUM_THREADS=$OMP_NUM_THREADS NCCL_DEBUG=INFO \
         python3 alphafold3_pytorch/train.py \
         data.batch_size=1 \
+        data.num_workers=1 \
+        data.prefetch_factor=1 \
         data.kalign_binary_path=/usr/bin/kalign \
         model.net.diffusion_num_augmentations=4 \
         +model.net.dim_atom=8 \
