a0e2531e0eb3b0f0e7ceb717c7f10884dbed4715
Alex Morehead <acmwhb@missouri.edu>
2024-09-15 10:28:16 -0500

    Try 38 with ROCm 6.2

--- scripts/slurm/af3_overfitting_experiment_e8.sh
+++ scripts/slurm/af3_overfitting_experiment_e8.sh
@@ -35,7 +35,7 @@ export RDZV_PORT=29400
 # For what `srun` is concerned, only one task is created, the `torchrun` process.
 
 # Define WandB run ID
-RUN_ID="141a7huv"  # NOTE: Generate a unique ID for each run using `python3 scripts/generate_id.py`
+RUN_ID="je6pnb26"  # NOTE: Generate a unique ID for each run using `python3 scripts/generate_id.py`
 
 # Run Singularity container
 srun -c 64 singularity exec \
@@ -47,6 +47,7 @@ srun -c 64 singularity exec \
     bash -c "
         /usr/bin/kalign --version \
         && WANDB_RESUME=allow WANDB_RUN_ID=$RUN_ID OMP_NUM_THREADS=$OMP_NUM_THREADS NCCL_DEBUG=INFO \
+        NCCL_DEBUG=INFO NCCL_DEBUG_SUBSYS=ALL TORCH_DISTRIBUTED_DEBUG=DETAIL TORCH_SHOW_CPP_STACKTRACES=1 \
         torchrun \
         --nnodes=$SLURM_JOB_NUM_NODES \
         --nproc_per_node=$NUM_PYTORCH_PROCESSES \
