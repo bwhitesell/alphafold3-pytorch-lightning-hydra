070af6ec82955582a3d05e788422f328925f60c4
Alex Morehead <acmwhb@missouri.edu>
2024-09-14 21:01:38 -0500

    Update ID

--- scripts/slurm/af3_initial_training.sh
+++ scripts/slurm/af3_initial_training.sh
@@ -38,7 +38,7 @@ echo "Rendezvous Node IP: $RDZV_HOST"
 # For what `srun` is concerned, only one task is created, the `torchrun` process.
 
 # Define WandB run ID
-RUN_ID="d60ux92u" # NOTE: Generate a unique ID for each run using `python3 scripts/generate_id.py`
+RUN_ID="dl7a93bs" # NOTE: Generate a unique ID for each run using `python3 scripts/generate_id.py`
 
 # Run Singularity container
 srun -c 64 singularity exec \
