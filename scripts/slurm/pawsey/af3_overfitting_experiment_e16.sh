392776e6d05465f766772b12a7543b36539b7d60
Alex Morehead <acmwhb@missouri.edu>
2024-09-15 21:30:08 -0500

    Update ID

--- scripts/slurm/pawsey/af3_overfitting_experiment_e16.sh
+++ scripts/slurm/pawsey/af3_overfitting_experiment_e16.sh
@@ -35,7 +35,7 @@ export RDZV_PORT=29400
 # For what `srun` is concerned, only one task is created, the `torchrun` process.
 
 # Define WandB run ID
-RUN_ID="gif0muu5"  # NOTE: Generate a unique ID for each run using `python3 scripts/generate_id.py`
+RUN_ID="ubyipeqn"  # NOTE: Generate a unique ID for each run using `python3 scripts/generate_id.py`
 
 # Run Singularity container
 srun -c 64 singularity exec \
